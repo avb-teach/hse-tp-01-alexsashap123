75db8b1f4f3c22914afde05a5a2bcfe8ed4f35ab
alexsashap123 <alexandrapapachistou@gmail.com>
2025-04-23 23:12:10 +0300

    Modified

--- collect_files.sh
+++ collect_files.sh
@@ -8,25 +8,16 @@ if [ "$1" == "--max_depth" ]; then
     input_dir=$3
     output_dir=$4
 fi
-
-# Создание выходной директории, если её нет
 mkdir -p "$output_dir"
-
-# Функция для копирования файлов с обработкой дубликатов
 copy_files() {
     local src="$1"
     local dest="$2"
     local current_depth="$3"
-    
-    # Проверка глубины
     if [ "$max_depth" -ne -1 ] && [ "$current_depth" -gt "$max_depth" ]; then
         return
     fi
-    
-    # Перебор элементов во входной директории
     for item in "$src"/*; do
         if [ -f "$item" ]; then
-            # Обработка файла
             filename=$(basename "$item")
             
             ext="${filename##*.}"
@@ -41,14 +32,10 @@ copy_files() {
             
             cp "$item" "$dest/$new_filename"
         elif [ -d "$item" ]; then
-            # Рекурсивный вызов для поддиректории
             copy_files "$item" "$dest" $((current_depth + 1))
         fi
     done
 }
-
-# Вызов функции копирования
 copy_files "$input_dir" "$output_dir" 0
-
 echo "Files collected successfully to $output_dir"
 
